bbeca018263f4bcc7ac7a42ac144f62c8a1eea1f
Ismael Avila <ismael.guada234015@gmail.com>
2021-09-17 17:22:57 -0500

    Definicion e implementacion de la clase cMaestro. Se agregó herencia de la clase cPersona y delegracion de constructores

--- cmaestro.cpp
+++ cmaestro.cpp
@@ -1,6 +1,9 @@
 #include "cmaestro.h"
 
-cMaestro::cMaestro()
-{
+cMaestro::cMaestro(std::string n, int c) : cPersona(n), codigo(c) {}
 
+void cMaestro::imprimir() {
+    std::cout << "Soy " << nombre << ". Mi codigo de maestro es: " << codigo << std::endl;
 }
+
+
--- cmaestro.h
+++ cmaestro.h
@@ -1,11 +1,18 @@
 #ifndef CMAESTRO_H
 #define CMAESTRO_H
 
+#include "cpersona.h"
 
-class cMaestro
-{
+class cMaestro : public cPersona {
+private:
+    int codigo;
+
 public:
-    cMaestro();
+    cMaestro(std::string n, int c);
+
+    void imprimir() override;
+
+
 };
 
 #endif // CMAESTRO_H
