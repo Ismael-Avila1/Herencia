0125f0a24f5bc454c8b483f81619b004daf53262
Ismael Avila <ismael.guada234015@gmail.com>
2021-09-18 00:04:25 -0500

    Definicion e implementacion de la clase cMotor. Se agregó objeto de la clase cAutoparte y delegacion de constructores

--- cmotor.cpp
+++ cmotor.cpp
@@ -1,6 +1,9 @@
 #include "cmotor.h"
 
-cMotor::cMotor()
-{
+cMotor::cMotor(int s, int n) : cAutoparte(s), numCilindros(n) {}
 
+void cMotor::imprimir() {
+    std::cout << "Numero de serie del motor: " << serie << "\n" << "Numero de cilindros: " << numCilindros << std::endl;
 }
+
+
--- cmotor.h
+++ cmotor.h
@@ -1,11 +1,16 @@
 #ifndef CMOTOR_H
 #define CMOTOR_H
 
+#include "cautoparte.h"
 
-class cMotor
-{
+class cMotor : public cAutoparte {
+private:
+    int numCilindros;
+
 public:
-    cMotor();
+    cMotor(int s, int n);
+
+    void imprimir() override;
 };
 
 #endif // CMOTOR_H
