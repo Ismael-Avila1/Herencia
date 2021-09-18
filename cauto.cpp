7deebedd7123244447c75c3ba817276e4a551aa4
Ismael Avila <ismael.guada234015@gmail.com>
2021-09-18 01:06:31 -0500

    Definicion e implementacion de la clase cAuto. Se agregó herencia de la clase cObjeto y delegacion de construtores. Inicializacion del motor en el constructor

--- cauto.cpp
+++ cauto.cpp
@@ -1,6 +1,10 @@
 #include "cauto.h"
 
-cAuto::cAuto()
-{
+cAuto::cAuto(int sm, int nc, int m) : motor(sm, nc), modelo(m) {}
 
+void cAuto::imprimir() {
+    std::cout << "Auto." << "\n" << "Modelo: " << modelo << std::endl;
+
+    std::cout <<"Datos del motor." << std::endl;
+    motor.imprimir();
 }
--- cauto.h
+++ cauto.h
@@ -1,11 +1,20 @@
 #ifndef CAUTO_H
 #define CAUTO_H
 
+#include <iostream>
 
-class cAuto
-{
+#include "cobjeto.h"
+#include "cmotor.h"
+
+class cAuto : public cObjeto {
+protected:
+    cMotor motor;
+    int modelo;
+
 public:
-    cAuto();
+    cAuto(int sm, int nc, int m);
+
+    void imprimir() override;
 };
 
 #endif // CAUTO_H
