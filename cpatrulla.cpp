557cdc6f821368086c5b1a48b0551159976cc597
Ismael Avila <ismael.guada234015@gmail.com>
2021-09-18 12:30:37 -0500

    Definicion e implementacion de la clase cPatrulla. Se agregó herencia de la clase cAuto y delegacion de constructores

--- cpatrulla.cpp
+++ cpatrulla.cpp
@@ -1,6 +1,14 @@
 #include "cpatrulla.h"
 
-cPatrulla::cPatrulla()
-{
+cPatrulla::cPatrulla(int sm, int nc, int m, bool s) : cAuto(sm, nc, m), sirena(s) {}
 
+void cPatrulla::imprimir() {
+    std::cout << "Patrulla." << "\n" << "Modelo: " << modelo << std::endl;
+
+    std::cout << "Datos del motor." << std::endl;
+    motor.imprimir();
+
+    std::cout << "La sirena está " << ((sirena == true) ? "encendida" : "apagada") << std::endl;
 }
+
+
