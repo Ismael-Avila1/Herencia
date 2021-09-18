a0665686506ecec4900e21205e0cf6815dc987b6
Ismael Avila <ismael.guada234015@gmail.com>
2021-09-18 12:48:00 -0500

    Definicion e implementacion de la clase cTaxi. Se agregó herencia de la clase cAuto y delegacion de constructores

--- ctaxi.cpp
+++ ctaxi.cpp
@@ -1,6 +1,14 @@
 #include "ctaxi.h"
 
-cTaxi::cTaxi()
-{
+cTaxi::cTaxi(int sm, int nc, int m, int s) : cAuto(sm, nc, m), sitio(s) {}
 
+void cTaxi::imprimir() {
+    std::cout << "Taxi." << "\n" << "Modelo: " << modelo << std::endl;
+
+    std::cout << "Datos del motor." << std::endl;
+    motor.imprimir();
+
+    std::cout << "Taxi del sitio: " << sitio << std::endl;
 }
+
+
--- ctaxi.h
+++ ctaxi.h
@@ -1,11 +1,15 @@
 #ifndef CTAXI_H
 #define CTAXI_H
 
+#include "cauto.h"
 
-class cTaxi
-{
+class cTaxi : public cAuto {
+private:
+    int sitio;
 public:
-    cTaxi();
+    cTaxi(int sm, int nc, int m, int s);
+
+    void imprimir() override;
 };
 
 #endif // CTAXI_H
